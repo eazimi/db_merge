7facc72f20a73fa03f2bb68b6c678c93cd99ddc7
eazimi <azimi.ehsan@hotmail.com>
2022-04-04 10:25:56 +0200

    sqlite3 support

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,21 @@
 
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
+#include <sqlite3.h>
 
 using namespace std;
 using boost::property_tree::ptree;
 
 int main(int argc, char* argv[])
 {
+    sqlite3 *db;
+    auto rc = sqlite3_open("../files/config-app.db3", &db);
+    if(rc)
+        cout << "Can't open database: " << sqlite3_errmsg(db) << endl;
+    else    
+        cout << "Opened database successfully" << endl; 
+
+    sqlite3_close(db);
+
     return 0;
 }
