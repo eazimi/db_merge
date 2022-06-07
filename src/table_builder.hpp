7eb56693d6b5d98ac50f2a276747bfe08b7d2b00
Ehsan Azimi <ehsan.azimi@kaco-newenergy.de>
2022-06-07 18:49:49 +0200

    implement dump_tbls()

--- src/table_builder.hpp
+++ src/table_builder.hpp
@@ -2,13 +2,84 @@
 #define TABLE_BUILDER_HPP
 
 #include "dump_builder.h"
+#include <sqlite3.h>
+#include <sstream>
 
 namespace Kaco
 {
     class DumpTableBuilder : public DumpBuilderBase
     {
+        using Self = DumpTableBuilder;
+
     public:
         DumpTableBuilder(Dump &dump) : DumpBuilderBase{dump} {}
+
+        Self &dump_tbls(sqlite3 *db)
+        {
+            int rc = sqlite3_prepare_v2(db, "SELECT sql,tbl_name FROM sqlite_master WHERE type = 'table';",
+                                     -1, &dump.stmt_table, nullptr);
+            if (rc != SQLITE_OK)
+            {
+                dump.cleanup();
+                return *this;
+            }
+
+            dump.oss << "PRAGMA foreign_keys=OFF;\nBEGIN TRANSACTION;\n";
+
+            rc = sqlite3_step(dump.stmt_table);
+            while (rc == SQLITE_ROW)
+            {
+                auto data = (const char *)sqlite3_column_text(dump.stmt_table, 0);
+                auto table_name = (const char *)sqlite3_column_text(dump.stmt_table, 1);
+                if (!data || !table_name)
+                {
+                    dump.cleanup();
+                    return *this;
+                }
+
+                /* CREATE TABLE statements */
+                dump.oss << data << endl;
+
+                /* fetch table data */
+                ostringstream ss;
+                ss << "SELECT * from " << table_name << ";";
+                rc = sqlite3_prepare_v2(db, ss.str().c_str(), -1, &dump.stmt_data, nullptr);
+                if (rc != SQLITE_OK)
+                {
+                    dump.cleanup();
+                    return *this;
+                }
+
+                rc = sqlite3_step(dump.stmt_data);
+                while (rc == SQLITE_ROW)
+                {
+                    // sprintf(cmd, "INSERT INTO %s VALUES(", table_name);
+                    ss.str("");
+                    ss << "INSERT INTO " << table_name << " VALUES(";
+                    auto col_cnt = sqlite3_column_count(dump.stmt_data);
+                    for (auto index = 0; index < col_cnt; index++)
+                    {
+                        if (index)
+                            ss << ",";
+                        data = (const char *)sqlite3_column_text(dump.stmt_data, index);
+
+                        if (data)
+                        {
+                            if (sqlite3_column_type(dump.stmt_data, index) == SQLITE_TEXT)
+                                ss << "'" << data << "'";  
+                            else
+                                ss << data;
+                        }
+                        else
+                            ss << "NULL";
+                    }
+                    dump.oss << ss.str() << ");\n";
+                    rc = sqlite3_step(dump.stmt_data);
+                }
+                rc = sqlite3_step(dump.stmt_table);
+            }
+            return *this;
+        }
     };
 
 } // namespace Kaco
